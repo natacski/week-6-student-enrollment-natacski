7ca4b6bfec3df726478929191569afe5dfa1ecb4
NATALIA ESPAÑA <natacski@gmail.com>
2020-07-22 10:42:07 -0400

    Adding edit button and comments

--- index.js
+++ index.js
@@ -7,9 +7,11 @@ let coursesURL = 'https://code-the-dream-school.github.io/JSONStudentsApp.github
 let studentsInfo = [];
 let coursesInfo = [];
 let submitNewStudent = document.getElementById('submit_btn');
+let editStudentButton = document.getElementById('edit-student-submit');
 
-
-// Event Listeners
+/*==========================================
+ Event Listeners
+===========================================*/
 students_button.addEventListener('click', () =>{
   showStudents();
 })
@@ -21,7 +23,10 @@ submitNewStudent.addEventListener('click', (e) => {
   addNewStudent();
 })
 
-//Getting all data and storing in one function
+
+/*============================================
+ Getting all data and storing in one function
+==============================================*/
 async function allData() {
   let response = await fetch(studentsURL);
   studentsInfo = await response.json();
@@ -40,8 +45,11 @@ async function allData() {
   document.getElementById('courses').disabled = false; 
 }
 
+/*======================================================
+STUDENTS BUTTON
+========================================================*/
 
-//Checking students status-making display the green circle
+ //Checking students status-making display the green circle
 function checkStudentStatus(studentStatus){
   if(studentStatus === true) {
     return '<span title="active student" class="dot"></span>'
@@ -50,7 +58,7 @@ function checkStudentStatus(studentStatus){
   }
 }
 
-//Displays students cards function
+//Displaying students cards with the data
 const showStudents = () =>{
   document.getElementById('students_data_body').hidden = false;
   document.getElementById('courses_data_body').hidden = true;
@@ -60,7 +68,6 @@ const showStudents = () =>{
     options += '<option value="' + course.id + '">' + course.name +  '</option>';  
   });
 
-
   students_body.innerHTML = ''; 
     studentsInfo.forEach(student => {
       let status = checkStudentStatus(student.status) ;
@@ -69,10 +76,10 @@ const showStudents = () =>{
       student.courses.forEach( course => {
         studentsCourses += "<li>" + course.name + "</li>"    
       });
-
+      //Creating the html cards for students
       students_body.innerHTML += ` <div class="container">
                                       <div class="col-sm-3  m-4 float-left">
-                                          <div class="card" style="width: 18rem;">
+                                          <div class="card" style="width: 18rem; height: 20rem;">
                                             <div class="card-body">
                                               <h5 class="card-title pt-4"> ${student.name}  ${student.last_name}  ${status} </h5>
                                               <p><ul> ${studentsCourses} </ul></p>
@@ -81,7 +88,7 @@ const showStudents = () =>{
                                                     ${options}
                                                 </select>
                                                   <button type="button" class="btn btn-outline-primary btn-sm mt-4 addCourse" studentId="${student.id}">Add course</button>
-                                                  <button type="button" class="btn btn-outline-primary btn-sm mt-4">Edit info</button>
+                                                  <button onclick="editStudent('${student.id}')" type="button" class="btn btn-outline-primary btn-sm mt-4 edit-info" studentId="${student.id}">Edit info</button>
                                               </div>
                                             </div>
                                           </div>
@@ -89,8 +96,8 @@ const showStudents = () =>{
                                    </div>`                        
   } ); 
 
-  
-//Add course button
+/*-----ADD COURSE BUTTON----------*/
+//displaying the list of courses available in the Add course button
 const addCoursesButtonClass= document.querySelectorAll('.addCourse');
   addCoursesButtonClass.forEach (el => el.addEventListener('click', event =>{
       let courseId = el.parentElement.getElementsByTagName('select')[0].value;
@@ -101,6 +108,7 @@ const addCoursesButtonClass= document.querySelectorAll('.addCourse');
   }));
 }
 
+//if statements 
 const addCourseToStudent = ( courseId, studentId ) => {
   let courseToAdd = coursesInfo.filter( course => course.id === parseInt(courseId))[0];
   let studentToAddTo = studentsInfo.filter( student => student.id === parseInt(studentId))[0];
@@ -128,15 +136,50 @@ const addCourseToStudent = ( courseId, studentId ) => {
 }
 
 
-//Displays courses cards
 
+/*-----EDIT STUDENTS INFO BUTTON----------*/
+editStudentButton.addEventListener('click', (e) =>{
+  let studentId = e.target.getAttribute('studentId');
+  let name = document.getElementById('name_edit').value;
+  let last_name = document.getElementById('lastName_edit').value;
+  let active = document.getElementById('status-drop-down').value;
+  let studentToEdit = studentsInfo.filter( student => student.id === parseInt(studentId))[0];
+
+  studentToEdit.name = name;
+  studentToEdit.last_name = last_name;
+  studentToEdit.status = active == "true";
+
+  $('#modal-edit-student').modal('hide');
+
+  showStudents();
+})
+
+
+//Edit info button-Student info with the existing information to display in modal form
+const editStudent = ( studentId ) =>{
+   let studentToEdit = studentsInfo.filter( student => student.id === parseInt(studentId))[0];
+
+  document.getElementById("edit-student-submit").setAttribute('studentId', studentId);
+  document.getElementById('name_edit').value = studentToEdit.name;
+  document.getElementById('lastName_edit').value = studentToEdit.last_name;
+  document.getElementById('status-drop-down').value = studentToEdit.status;
+
+  $('#modal-edit-student').modal('show');
+}
+
+
+/*======================================================
+COURSES BUTTON
+========================================================*/
+
+//Displaying couses cards with the data
 const showCourses = () =>{
   document.getElementById('students_data_body').hidden = true;
   document.getElementById('courses_data_body').hidden = false;
 
   let options = ''
   studentsInfo.forEach(student => {
-    options += '<option value="' + student.id + '">' + student.name +  student.last_name +  '</option>';  
+    options += '<option value="' + student.id + '">' + student.name + ' ' +  student.last_name +  '</option>';  
   });
 
     courses_body.innerHTML = ''; 
@@ -144,7 +187,7 @@ const showCourses = () =>{
       let allStudents = '';
 
       course.students.forEach( student => {
-        allStudents += "<li>" + student.name + student.last_name +"</li>"    
+        allStudents += "<li>" + student.name + ' ' +  student.last_name +"</li>"    
       });
 
       courses_body.innerHTML += ` <div class="container">
@@ -165,7 +208,7 @@ const showCourses = () =>{
                                     </div>` 
 } ); 
 
-//Displays Add student button
+/*-----ADD STUDENT BUTTON----------*/
 const addStudentButtonClass= document.querySelectorAll('.addStudent');
   addStudentButtonClass.forEach (el => el.addEventListener('click', event =>{
       let studentId = el.parentElement.getElementsByTagName('select')[0].value;
@@ -201,46 +244,22 @@ const addStudentToCourse = ( studentId, courseId ) => {
   showCourses();
 }
 
-//Add new student function
-
+/*======================================================
+NEW STUDENT BUTTON
+========================================================*/
+//Adding new student function
 const addNewStudent = () =>{
   
   const name = document.getElementById('name').value;
   const last_name = document.getElementById('lastName').value;
   
-  //push the new student
   studentsInfo.push({id: studentsInfo.length, name: name, last_name: last_name, status:true, courses:[]});
-  //refresh the student cards
-
+  
   $('#new_student').modal('hide');
   showStudents();
 }
 
 
 allData();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
 
 
